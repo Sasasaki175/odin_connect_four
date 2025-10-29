880416a7e2fd647119d0a31d4ed5ea64525717eb
Sasasaki175 <207643966+Sasasaki175@users.noreply.github.com>
2025-10-29 10:46:40 +0900

    Add rspec to Gemfile and create stubs for testing methods in the spec file

---
+++ spec/connect_four_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../connect_four'
+
+describe ConnectFour do
+  subject(:game) { described_class.new }
+
+  describe '#start_game' do
+
+  end
+
+  describe '#player_move' do
+  end
+
+  describe '#game_over?' do
+  end
+
+  describe '#check_winner' do
+  end
+end
